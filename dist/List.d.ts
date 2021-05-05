040c3caf87f45877d804ee2f1344be2a79da59d9
Hiroki Osame <hiroki.osame@gmail.com>
2021-05-05 19:27:06 -0400

    built

--- dist/List.d.ts
+++
@@ -1,3 +0,0 @@
-import { FC } from 'react';
-declare const List: FC;
-export = List;
--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ListItem = exports.List = void 0;
-var List_1 = require("./List");
-Object.defineProperty(exports, "List", { enumerable: true, get: function () { return __importDefault(List_1).default; } });
-var ListItem_1 = require("./ListItem");
-Object.defineProperty(exports, "ListItem", { enumerable: true, get: function () { return __importDefault(ListItem_1).default; } });
+exports.Task = exports.TaskList = void 0;
+var TaskList_1 = require("./TaskList");
+Object.defineProperty(exports, "TaskList", { enumerable: true, get: function () { return __importDefault(TaskList_1).default; } });
+var Task_1 = require("./Task");
+Object.defineProperty(exports, "Task", { enumerable: true, get: function () { return __importDefault(Task_1).default; } });
